61c5d2bab52e785b9d5a9b7f76e685d21f33e97b
Rainey <changjiu1391@126.com>
2021-10-10 17:49:41 +0800

    根据role动态渲染menu和动态添加路由

--- src/router/index.js
+++ src/router/index.js
@@ -177,7 +177,7 @@ if(to.meta.auth){
 //判断登录状态
 
 if(status=='unlogin'){
-   //如果未登录，跳转到login页面，并携带目标页面的参数 ，登录成功跳转到目标页面
+   //如果未登录，跳转到login页面，并 携带目标页面的参数 ，以便登录成功后跳转到目标页面
   next({
           name:'Login',
          query:{
@@ -185,7 +185,7 @@ if(status=='unlogin'){
           }
       }); 
    }
-   //如果已经登录（或登录信息未失效），恢复登录状态
+   //如果已经登录（或登录信息未失效），恢复登录状态 并 跳转到目标页面
   else{
     if(store.state.user.user=='') store.commit('user/recoverLoginState');
 
@@ -217,3 +217,16 @@ router.afterEach((to,from)=>{
 });
 
 export default router
+// 🌈师 ：懂小姐  之    鉴权 登录  恢复登录状态 
+ // 鉴权： 如果不是login页面，其他页面都需要鉴权
+// 设置登录 和 登出： 同时操作 用户登录信息  和 本地存储  ，并且改变仓库状态 都是使用的是  dispach （触发action ，师的理由是，这样devtool可以监控到仓库状态变化，否则，有些状态变化监控不到）
+// 恢复登录状态 ： 网站初始化的时候 直接读取本地存储为用户登录信息  ，如果读到了，就是 login状态， 如果没读到，就是unlogin状体。 达到了自动恢复登录状态的目的。
+//本地存储使用 cookie  ， 并使用 js-cookie 第三方库 来操作cookie。
+//用户的登录信息user 的每个字段分别存到一个cookie。    而我，是直接整体作为一个字段 user 存储到一个 cookie/localStaorge
+
+
+//师： 董小姐 ，根据用户role ，动态分配路由。  ： 根据用户权限，动态渲染菜单栏。 
+
+
+
+
--- src/store/setting.js
+++ src/store/setting.js
@@ -5,9 +5,13 @@ export default {
      namespaced:true,
 
      state: {
+          //🌈师，董小姐 ，把menuData 信息都放到了（融合到了） 路由的 meta里面了，  因为  菜单项 和 （页面）路由是有对应关系的。
+          // 根据role 动态渲染 菜单栏 ： 首先根据登录用户role过滤路由，得到allowRoutes ，concat 到 路由规则里面去。 
+           //                       然后（如果有需要），过滤menuData ，因为有些路由的 meta.title 不需要显示到菜单栏，例如 登录。 
           menuData: [
 
-
+          //    用了 bstp 和 阿里 的icon-font   ， 师傅，董小姐 用了  ant design 的 icon 。 
+          // 🌈小结 ： 基于vue ， 开发就是 ， 面向数据 ， 面向npm ， 面向组件库。 
                {
                     title: "主页",  
                     iconClass: "iconfont icon-zhuye",
